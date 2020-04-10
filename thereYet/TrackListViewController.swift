09cb4f7ab9c0d308784dfb1550cc69802c27989d
DHwty <clemwek@gmail.com>
2020-04-10 20:13:43 +0300

    Add tableview and some manipulations

---
+++ thereYet/TrackListViewController.swift
@@ -0,0 +1,60 @@
+//
+//  TrackListViewController.swift
+//  thereYet
+//
+//  Created by Clement Wekesa on 10/04/2020.
+//  Copyright © 2020 ddhwty. All rights reserved.
+//
+
+import UIKit
+
+class TrackListViewController: UIViewController {
+
+    @IBOutlet weak var trackTable: UITableView!
+    
+    var places: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        places = ["test", "test1", "test2", "test3", "test4"]
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension TrackListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return places.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "trackListCell", for: indexPath)
+        cell.textLabel?.text = places[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            places.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+}
